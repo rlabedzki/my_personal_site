6b858b98bf3637f2304d203cc248954a4b9c857c
rlabedzki <rlabedzki@gmail.com>
2025-09-24 15:38:10 +0200

    deploy

--- tests_e2e/tests/test_homepage.py
+++ tests_e2e/tests/test_homepage.py
@@ -3,5 +3,5 @@ from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
     # Tu zmień na adres swojego Flaska, np. jak odpalasz lokalnie na 5000
-    page.goto("http://127.0.0.1:5000/")
+    page.goto("http://127.0.0.1:80/")
     expect(page).to_have_title("Rafał Łabędzki")
